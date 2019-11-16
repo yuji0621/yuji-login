732d574b4c89071b7efac0d138b35e8b8ecb4ebb
yuji.sonobe <yui.kuumi@gmail.com>
2019-11-16 08:36:21 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,14 +43,14 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
 
-private
+  private
 
-def set _task
-  @tassk = Task.find(params[:id])
-end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-def task_params
-  params.require(:task).permit(:content, :status)
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
